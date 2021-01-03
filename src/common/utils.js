d96fa0be70f44692a414ef21bbd3f3a700c1187d
serhiirodikov <sergiy1998373@gmail.com>
2021-01-03 20:50:21 +0200

    [ADDED] login page, change speciality and admin pa

--- src/common/utils.js
+++ src/common/utils.js
@@ -7,15 +7,22 @@ export const addEntity = (entity, payload) => {
 export const deleteEntity = (entity, id) => {
   return axios.get(`${environment.BASEURL}${entity}/del/${id}`)
 }
-export const login = () => {
-  return axios.post(`${environment.BASEURL}login/index`, {
-    username: 'admin',
-    password: 'dtapi_admin',
-  })
+export const login = (body) => {
+  return axios.post(`${environment.BASEURL}login/index`, body)
 }
-export const getEntityData = (entity) => {
-  return axios.get(`${environment.BASEURL}${entity}/getRecords/`)
+export const getEntityData = (entity, source) => {
+  return axios.get(`${environment.BASEURL}${entity}/getRecords/`, {
+    cancelToken: source.token,
+  })
 }
 export const updateEntity = (entity, id, payload) => {
   return axios.post(`${environment.BASEURL}${entity}/update/${id}`, payload)
+}
+
+export const isLogged = () => {
+  return axios.get(`${environment.BASEURL}login/isLogged`)
+}
+
+export const logOut = () => {
+  return axios.get(`${environment.BASEURL}login/logout`)
 }
---
+++ src/components/login/Login.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react'
+
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import { isLogged, login } from '../../common/utils'
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}))
+
+const Login = ({ setAuthInfo }) => {
+  const classes = useStyles()
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    const logInfo = { username: name, password }
+    await login(logInfo)
+    const isAuth = await isLogged()
+    setAuthInfo(isAuth)
+  }
+
+  const nameChangeHandle = (e) => {
+    setName(e.target.value)
+  }
+  const passwordChangeHandle = (e) => {
+    setPassword(e.target.value)
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Log in
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={submitHandler}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Name"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={name}
+            onChange={nameChangeHandle}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            autoComplete="current-password"
+            onChange={passwordChangeHandle}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Log In
+          </Button>
+        </form>
+      </div>
+    </Container>
+  )
+}
+
+export default Login
