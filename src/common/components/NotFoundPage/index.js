44ab7c6f758ddebbf35331752d21052c6337c754
MaxVein <58347366+MaxVein@users.noreply.github.com>
2021-01-13 00:10:48 +0200

    Completed changable 404 page

--- src/common/components/NotFoundPage/index.js
+++ src/common/components/NotFoundPage/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import NotFoundImageComponent from './NotFoundImageComponent';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    content: {
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+        userSelect: 'none',
+    },
+}));
 
 export default function NotFoundPage() {
-    return <div>404</div>;
+    const classes = useStyles();
+    return (
+        <Container className={classes.container}>
+            <div className={classes.content}>
+                <NotFoundImageComponent />
+            </div>
+        </Container>
+    );
 }
