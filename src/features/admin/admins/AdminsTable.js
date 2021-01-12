37521dfc43b1201cf391183c469054e094aa4940
MaxVein <58347366+MaxVein@users.noreply.github.com>
2021-01-12 22:33:25 +0200

    Added Loader

--- src/features/admin/admins/AdminsTable.js
+++ src/features/admin/admins/AdminsTable.js
@@ -20,17 +20,20 @@ import AdminCreationForm from './AdminsCreationForm';
 import AdminsTableRow from './AdminTableRow';
 import AdminsContext from './AdminsContext';
 import SnackbarHandler from '../../../common/snackbar';
+import Loader from '../../../components/Loader/Loader';
 
 export default function AdminsTable() {
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
     const [dataSource, setDataSource] = React.useState([]);
+    const [loaded, setLoaded] = React.useState(false);
     const [open, setOpen] = React.useState(false);
     const [snack, setSnack] = React.useState({ open: false, message: '', type: 'success' });
 
     useEffect(() => {
         getAdmins().then((res) => {
             setDataSource(createDataSource(res));
+            setLoaded(true);
         });
     }, []);
     const handleChangePage = (event, newPage) => {
@@ -48,72 +51,76 @@ export default function AdminsTable() {
 
     return (
         <AdminsContext.Provider value={{ dataSource, setDataSource, snack, setSnack }}>
-            <React.Fragment>
-                <div className={styles.entityHeader}>
-                    <Typography
-                        component="h2"
-                        variant="h4"
-                        color="textPrimary"
-                        className={styles.entityHeaderTitle}
-                    >
-                        <SupervisedUserCircle fontSize="large" />
-                        Адміни
-                    </Typography>
-                    <Button
-                        onClick={openModal}
-                        disableElevation
-                        variant="contained"
-                        color="primary"
-                        className={styles.entityHeaderButton}
-                    >
-                        <AddCircle />
-                        Додати Адміна
-                    </Button>
-                </div>
-                <Paper elevation={6}>
-                    <TableContainer className={styles.entityTableContainer}>
-                        <Table stickyHeader className={styles.entityTable}>
-                            <TableHead>
-                                <TableRow>
-                                    {columns.map((column) => (
-                                        <TableCell
-                                            key={column.id}
-                                            align={column.align}
-                                            style={{ minWidth: column.minWidth }}
-                                        >
-                                            {column.label}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {dataSource
-                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                    .map((admin) => {
-                                        return <AdminsTableRow key={admin.id} admin={admin} />;
-                                    })}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                    <TablePagination
-                        rowsPerPageOptions={[10, 25, 100]}
-                        component="div"
-                        count={dataSource.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        onChangePage={handleChangePage}
-                        onChangeRowsPerPage={handleChangeRowsPerPage}
+            {loaded ? (
+                <React.Fragment>
+                    <div className={styles.entityHeader}>
+                        <Typography
+                            component="h2"
+                            variant="h4"
+                            color="textPrimary"
+                            className={styles.entityHeaderTitle}
+                        >
+                            <SupervisedUserCircle fontSize="large" />
+                            Адміни
+                        </Typography>
+                        <Button
+                            onClick={openModal}
+                            disableElevation
+                            variant="contained"
+                            color="primary"
+                            className={styles.entityHeaderButton}
+                        >
+                            <AddCircle />
+                            Додати Адміна
+                        </Button>
+                    </div>
+                    <Paper elevation={6}>
+                        <TableContainer className={styles.entityTableContainer}>
+                            <Table stickyHeader className={styles.entityTable}>
+                                <TableHead>
+                                    <TableRow>
+                                        {columns.map((column) => (
+                                            <TableCell
+                                                key={column.id}
+                                                align={column.align}
+                                                style={{ minWidth: column.minWidth }}
+                                            >
+                                                {column.label}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {dataSource
+                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                        .map((admin) => {
+                                            return <AdminsTableRow key={admin.id} admin={admin} />;
+                                        })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <TablePagination
+                            rowsPerPageOptions={[10, 25, 100]}
+                            component="div"
+                            count={dataSource.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onChangePage={handleChangePage}
+                            onChangeRowsPerPage={handleChangeRowsPerPage}
+                        />
+                    </Paper>
+                    <AdminCreationForm
+                        open={open}
+                        setOpen={setOpen}
+                        mode={'Add'}
+                        dataSource={dataSource}
+                        setDataSource={setDataSource}
                     />
-                </Paper>
-                <AdminCreationForm
-                    open={open}
-                    setOpen={setOpen}
-                    mode={'Add'}
-                    dataSource={dataSource}
-                    setDataSource={setDataSource}
-                />
-                <SnackbarHandler snack={snack} setSnack={setSnack} />
-            </React.Fragment>
+                    <SnackbarHandler snack={snack} setSnack={setSnack} />
+                </React.Fragment>
+            ) : (
+                <Loader />
+            )}
         </AdminsContext.Provider>
     );
 }
