904dba571bbc7a85797abea7c4bbde7d9941b17e
MaxVein <58347366+MaxVein@users.noreply.github.com>
2021-01-18 11:24:22 +0200

    Refactored Admins Component (#41)

--- src/features/admin/admins/AdminsService.js
+++ src/features/admin/admins/AdminsService.js
@@ -40,3 +40,121 @@ export async function checkAdminName(username) {
         .get(`${environment.BASEURL}AdminUser/checkUserName/${username}`)
         .then((res) => res.data);
 }
+
+export function addModeSubmit(data, setSnack, setDataSource, t, closeModal) {
+    checkAdminName(data.username)
+        .then((res) => {
+            if (res.response) {
+                setSnack({
+                    open: true,
+                    message: t('admins.messages.nameFailed'),
+                    type: 'warning',
+                });
+                return null;
+            }
+            addAdmin(data)
+                .then((res) => {
+                    setDataSource((prevVal) => prevVal.concat(res.data));
+                    setSnack({
+                        open: true,
+                        message: t('admins.messages.addSuccess'),
+                        type: 'success',
+                    });
+                    closeModal();
+                })
+                .catch((err) =>
+                    setSnack({
+                        open: true,
+                        message: `${t('admins.messages.serverFailed')}${err}`,
+                        type: 'error',
+                    }),
+                );
+        })
+        .catch(() =>
+            setSnack({
+                open: true,
+                message: t('admins.messages.nameServerFailed'),
+                type: 'error',
+            }),
+        );
+}
+
+export function updateModeSubmit(data, setSnack, setDataSource, t, closeModal) {
+    if (
+        data.values.username === data.intialFormValues.username &&
+        data.values.email === data.intialFormValues.email &&
+        data.values.password === data.intialFormValues.password
+    ) {
+        setSnack({
+            open: true,
+            message: t('admins.messages.changeNeeded'),
+            type: 'info',
+        });
+    } else {
+        checkAdminName(data.values.username)
+            .then((res) => {
+                if (res.response) {
+                    setSnack({
+                        open: true,
+                        message: t('admins.messages.nameFailed'),
+                        type: 'error',
+                    });
+                } else {
+                    updateAdmin(data.values, data.id)
+                        .then((res) => {
+                            if (res.response === 'ok') {
+                                setDataSource((prevVal) =>
+                                    prevVal.map((item) =>
+                                        item.id === data.id
+                                            ? (item = { id: data.id, ...data.values })
+                                            : item,
+                                    ),
+                                );
+                                closeModal();
+                                setSnack({
+                                    open: true,
+                                    message: t('admins.messages.updateSuccess'),
+                                    type: 'success',
+                                });
+                            }
+                        })
+                        .catch((err) =>
+                            setSnack({
+                                open: true,
+                                message: `${t('admins.messages.serverFailed')}${err}`,
+                                type: 'error',
+                            }),
+                        );
+                }
+            })
+            .catch(() =>
+                setSnack({
+                    open: true,
+                    message: t('admins.messages.nameServerFailed'),
+                    type: 'error',
+                }),
+            );
+    }
+}
+
+export function deleteModeSubmit(id, setSnack, setDataSource, t, closeModal) {
+    deleteAdmin(id)
+        .then((res) => {
+            if (res.response === 'ok') {
+                setDataSource((prevVal) => prevVal.filter((tableAdmin) => tableAdmin.id !== id));
+                closeModal();
+                setSnack({
+                    open: true,
+                    message: t('admins.messages.deleteSuccess'),
+                    type: 'success',
+                });
+            }
+        })
+        .catch((err) =>
+            setSnack({
+                open: true,
+                message: `${t('admins.messages.serverFailed')}${err}`,
+                type: 'error',
+            }),
+        );
+}
